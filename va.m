f1ed820d0969a45f4ec02ed995a365c2c28bae41
athabasca <athabasca@users.noreply.github.com>
2017-04-08 01:53:17 -0700

    actually add va.m >_>

---
+++ va.m
@@ -0,0 +1,22 @@
+function [ v, a, dn ] = va(folder)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+% Switch to folder, switch back later.
+prevd = pwd; 
+cd(folder);
+
+% Get filenames.
+dd = dir('*.wav');
+dn = {dd.name};
+n = length(dd);
+% Compute valence and arousal for all songs in folder.
+v = zeros(1,n); % valence
+a = zeros(1,n); % arousal
+for i = 1:n;
+    f = featurex(dn{i});
+    v(i) = f.valence;
+    a(i) = f.arousal;
+end
+cd(prevd);
+end
