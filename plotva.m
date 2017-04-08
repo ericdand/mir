3218f57574a820519604f0558221c6dda69fdb25
athabasca <athabasca@users.noreply.github.com>
2017-04-08 01:52:45 -0700

    Split plotva into va and plotva. no more computing v and a every time i change my plotting code

--- plotva.m
+++ plotva.m
@@ -1,25 +1,24 @@
-function [ v, a, dn ] = plotva(folder)
+function plotva(v,a,dn)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-% Switch to folder, switch back later.
-prevd = pwd; 
-cd(folder);
-
-% Get filenames.
-dd = dir('*.wav');
-dn = {dd.name};
-n = length(dd);
-% Compute valence and arousal for all songs in folder.
-v = zeros(1,n); % valence
-a = zeros(1,n); % arousal
-for i = 1:n;
-    f = aafeaturex(dn{i});
-    v(i) = f.valence;
-    a(i) = f.arousal;
-end
-cd(prevd);
-% Plot V-A. No labels yet.
 scatter(v,a);
+dx = 0.2; dy = 0.2;
+text(v+dx,a+dx,dn);
+xlabel('Valence');
+ylabel('Arousal');
+
+xmax = max(abs(v));
+xmax = 1.2*xmax;
+ymax = max(abs(a));
+ymax = 1.2*ymax;
+axis([-xmax xmax -ymax ymax]);
+
+x = -xmax:xmax;
+y = -ymax:ymax;
+hold on;
+plot(0*y,y,'black');
+hold on;
+plot(x,0*x,'black');
 end
 
