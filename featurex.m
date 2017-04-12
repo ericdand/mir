afc68b6f01ae827148700c9b01653bf818cebde9
athabasca <athabasca@users.noreply.github.com>
2017-04-11 23:58:32 -0700

    separate the sigmoid function tumor from featurex

--- featurex.m
+++ featurex.m
@@ -41,10 +41,6 @@ features.timbralwidth = mean(obj.output{8}.Data);
 features.volume = mean(obj.output{9}.Data);
 features.dissonance = mean(obj.output{12}.Data);
 
-function sig = sigmoid(n) 
-    sig = 1 / (1 + exp(-n));
-end
-
 % AROUSAL
 % arousal = sigmoid(centroid + chord) * loudness / mode
 % Assuming sigmoid is the logistic function logsig(n) = 1 / (1 + exp(-n)
@@ -53,4 +49,8 @@ features.arousal = sigmoid(features.centroid) * features.loudness / features.mod
 % VALENCE
 % valence = beat sum - dissonance / (timbral width + volume)
 features.valence = (features.beatsum - features.dissonance) / (features.timbralwidth + features.volume);
+end
 
+function sig = sigmoid(n) 
+    sig = 1 / (1 + exp(-n));
+end
