3eafa84243399a5367254b65cad0205e142ac720
Marin Evergreen <marinrevergreen@gmail.com>
2017-04-18 00:18:24 -0700

    Fixed bug with distance and removed disp commands

--- simpleRecommender.m
+++ simpleRecommender.m
@@ -1,15 +1,9 @@
 function recom = simpleRecommender(queryDir, songsetDir)
 % Call va.m with the queryDir
 [qa qv qn] = va(queryDir);
-disp(qa);
-disp(qv);
-disp(qn);
 
 % Call va.m with the songsetDir
 [sa sv sn] = va(songsetDir);
-disp(sa);
-disp(sv);
-disp(sn);
 
 % Determine the distance between the query songs and each song in songset
 % First, find the average v-a values for the query songs
@@ -18,9 +12,8 @@ qv = sum(qv)/size(qv,2);
 
 % Initialize a distance array
 dist = zeros(size(sn));
-
 for i=1:size(sn,2)
-    dist = euclideanDistance([qa qv],[sa(i) sv(i)]);
+    dist(i) = euclideanDistance([qa qv],[sa(i) sv(i)]);
 end
 
 % Find minimum value
